2a028af2b5235edc8f54d5189006511a46255b7b
alisradwan <aloshradwan44@gmail.com>
2022-11-20 18:17:40 -0800

    models setup done

---
+++ models/ContactInfo.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class ContactInfo extends Model {}
+
+ContactInfo.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Zip_Code: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    City: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    State: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Phone_Number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "ContactInfo",
+  }
+);
+
+module.exports = ContactInfo;
---
+++ models/index.js
@@ -0,0 +1,52 @@
+const User = require("./User");
+const ContactInfo = require("./ContactInfo");
+const Education = require("./Education");
+const Skills = require("./Skills");
+const Summary = require("./Summary");
+const WorkExperience = require("./WorkExperience");
+
+//create associations
+User.hasMany(ContactInfo, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+User.hasMany(Education, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+User.hasMany(Skills, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+User.hasMany(Summary, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+User.hasMany(WorkExperience, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+
+ContactInfo.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+
+Education.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+Skills.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+Summary.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+WorkExperience.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+
+module.exports = { User, Post, Comment };
