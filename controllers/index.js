a57cc9a9bb64e5e6404f47ed6478ab2d1131827a
alisradwan <aloshradwan44@gmail.com>
2022-12-04 23:16:56 -0800

    heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,27 @@
 //DEPENDENCY//
 const router = require("express").Router();
 //IMPORT API ROUTES --> SEPARATELY//
-    const apiRoutes = require("./api");
-    const loginRoutes = require("./loginRoutes");
-    const resumeRoutes = require("./resumeRoutes");
-    const signupRoutes = require("./signupRoutes");
-    const logoutRoutes = require("./logoutRoutes");
-    const dashboardRoutes = require("./dashboard-routes");
-    const editRoutes = require("./editRoutes");
-    const addResume = require("./addResume");
-    const homeRoute = require('./homeRoutes');
-    const userEdit = require("./userEditRoutes");
+const apiRoutes = require("./api");
+const loginRoutes = require("./loginRoutes");
+const resumeRoutes = require("./resumeRoutes");
+const signupRoutes = require("./signupRoutes");
+const logoutRoutes = require("./logoutRoutes");
+const dashboardRoutes = require("./dashboard-routes");
+const editRoutes = require("./editRoutes");
+const addResume = require("./addResume");
+const homeRoute = require("./homeRoutes");
+const userEdit = require("./userEditRoutes");
 
 //ROUTE --> CONTROLLER//
-    router.use("/api", apiRoutes);
-    router.use('/resume', resumeRoutes);
-    router.use("/login", loginRoutes);
-    router.use("/signup", signupRoutes);
-    router.use("/logout", logoutRoutes);
-    router.use("/dashboard", dashboardRoutes);
-    router.use("/edit", editRoutes);
-    router.use("/addresume", addResume);
-    router.use('/', homeRoute);
-    router.use("/useredit", userEdit);
-   
-    module.exports = router;
+router.use("/api", apiRoutes);
+router.use("/resume", resumeRoutes);
+router.use("/login", loginRoutes);
+router.use("/signup", signupRoutes);
+router.use("/logout", logoutRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/edit", editRoutes);
+router.use("/addResume", addResume);
+router.use("/", homeRoute);
+router.use("/useredit", userEdit);
+
+module.exports = router;
